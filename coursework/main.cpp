3948076c342b74577d5fc9222b1a73835ca4c8d0
Попехин Константин Сергеевич <popekhin80@bk.ru>
2025-08-10 12:13:11 +0700

    Initial Commit

---
+++ coursework/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  coursework
+//
+//  Created by Константин Попехин on 10.08.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
